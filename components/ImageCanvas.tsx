61865e40342a9440d3340fd38444d4831207d85f
ehayng1 <51578896+ehayng1@users.noreply.github.com>
2024-01-17 22:49:58 -0600

    first commit

--- components/ImageCanvas.tsx
+++ components/ImageCanvas.tsx
@@ -1,7 +1,7 @@
-import { useRef, useState } from 'react';
-import { IMAGE_URLS } from '../data/sample-image-urls';
-import { inferenceSqueezenet } from '../utils/predict';
-import styles from '../styles/Home.module.css';
+import { useRef, useState } from "react";
+import { IMAGE_URLS } from "../data/sample-image-urls";
+import { inferenceSqueezenet } from "../utils/predict";
+import styles from "../styles/Home.module.css";
 
 interface Props {
   height: number;
@@ -9,22 +9,21 @@ interface Props {
 }
 
 const ImageCanvas = (props: Props) => {
-
   const canvasRef = useRef<HTMLCanvasElement>(null);
   var image: HTMLImageElement;
   const [topResultLabel, setLabel] = useState("");
   const [topResultConfidence, setConfidence] = useState("");
   const [inferenceTime, setInferenceTime] = useState("");
-  
+
   // Load the image from the IMAGE_URLS array
   const getImage = () => {
     var sampleImageUrls: Array<{ text: string; value: string }> = IMAGE_URLS;
     var random = Math.floor(Math.random() * (9 - 0 + 1) + 0);
     return sampleImageUrls[random];
-  }
+  };
 
   // Draw image and other  UI elements then run inference
-  const displayImageAndRunInference = () => { 
+  const displayImageAndRunInference = () => {
     // Get the image
     image = new Image();
     var sampleImage = getImage();
@@ -37,19 +36,18 @@ const ImageCanvas = (props: Props) => {
 
     // Draw the image on the canvas
     const canvas = canvasRef.current;
-    const ctx = canvas!.getContext('2d');
+    const ctx = canvas!.getContext("2d");
     image.onload = () => {
       ctx!.drawImage(image, 0, 0, props.width, props.height);
-    }
-   
+    };
+
     // Run the inference
     submitInference();
   };
 
   const submitInference = async () => {
-
     // Get the image data from the canvas and submit inference.
-    var [inferenceResult,inferenceTime] = await inferenceSqueezenet(image.src);
+    var [inferenceResult, inferenceTime] = await inferenceSqueezenet(image.src);
 
     // Get the highest confidence.
     var topResult = inferenceResult[0];
@@ -58,23 +56,21 @@ const ImageCanvas = (props: Props) => {
     setLabel(topResult.name.toUpperCase());
     setConfidence(topResult.probability);
     setInferenceTime(`Inference speed: ${inferenceTime} seconds`);
-
   };
 
   return (
     <>
-      <button
-        className={styles.grid}
-        onClick={displayImageAndRunInference} >
-        Run Squeezenet inference
+      <button className={styles.grid} onClick={displayImageAndRunInference}>
+        Run Squeezenet inferences
       </button>
-      <br/>
+      <br />
       <canvas ref={canvasRef} width={props.width} height={props.height} />
-      <span>{topResultLabel} {topResultConfidence}</span>
+      <span>
+        {topResultLabel} {topResultConfidence}
+      </span>
       <span>{inferenceTime}</span>
     </>
-  )
-
+  );
 };
 
 export default ImageCanvas;
